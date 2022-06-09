2a4d5ec6877e726b6788c7c2a104b1fa003e7e82
phonesavanhDEV <phonesavanh6912@gmail.com>
2022-06-09 11:04:28 +0700

    9-6-2022

--- lib/menu/home.dart
+++ lib/menu/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Home extends StatefulWidget {
   static const routeName = '/';
@@ -28,3 +29,27 @@ class _HomeState extends State<Home> {
     );
   }
 }
+
+// class _HomeState extends State<Home> {
+//   CarouselController buttonCarouselController = CarouselController();
+
+//   @override
+//   Widget build(BuildContext context) => Column(children: <Widget>[
+//         CarouselSlider(
+//           items: null,
+//           carouselController: buttonCarouselController,
+//           options: CarouselOptions(
+//             autoPlay: false,
+//             enlargeCenterPage: true,
+//             viewportFraction: 0.9,
+//             aspectRatio: 2.0,
+//             initialPage: 2,
+//           ),
+//         ),
+//         RaisedButton(
+//           onPressed: () => buttonCarouselController.nextPage(
+//               duration: Duration(milliseconds: 300), curve: Curves.linear),
+//           child: Text('→'),
+//         )
+//       ]);
+// }
