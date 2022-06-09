8842db1642bb8ebc18768fc1a471bf77651d8cfa
phonesavanhDEV <phonesavanh6912@gmail.com>
2022-06-09 09:56:10 +0700

    9-6-2022

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './menu/launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,68 +7,23 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-       
-        primarySwatch: Colors.deepOrange,
-      ),
-      home: const MyHomePage(title: 'SVN2022'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
- 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'click nice',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.deepOrange,
+        ).copyWith(
+          secondary: Colors.deepOrange,
         ),
+        textTheme: TextTheme(bodyText2: TextStyle(color: Colors.red)),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      title: 'First Flutter App',
+      initialRoute: '/',
+      routes: {
+        Launcher.routeName: (context) => Launcher(),
+      },
     );
   }
 }
---
+++ lib/menu/launcher.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'home.dart';
+//import 'contact.dart';
+//import 'profile.dart';
+//import 'about.dart';
+//import 'settings.dart';
+
+class Launcher extends StatefulWidget {
+  static const routeName = '/';
+
+  const Launcher({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _LauncherState();
+  }
+}
+
+class _LauncherState extends State<Launcher> {
+  int _selectedIndex = 0;
+  final List<Widget> _pageWidget = <Widget>[
+    const Home(),
+    //const About(),
+   //const Profile(),
+    //const Contact(),
+    //const Settings(),
+  ];
+  final List<BottomNavigationBarItem> _menuBar = <BottomNavigationBarItem>[
+    BottomNavigationBarItem(
+      icon: Icon(FontAwesomeIcons.home),
+      label: 'Home',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(FontAwesomeIcons.infoCircle),
+      label: 'About',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(FontAwesomeIcons.userAlt),
+      label: 'Profile',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(FontAwesomeIcons.addressCard),
+      label: 'Contact',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(FontAwesomeIcons.cog),
+      label: 'Settings',
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pageWidget.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _menuBar,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
