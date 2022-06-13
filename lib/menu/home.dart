a9cb265820d392d301c848948f124e5ee0634502
phonesavanhDEV <phonesavanh6912@gmail.com>
2022-06-13 16:42:11 +0700

    13-6-2022edit

--- lib/menu/home.dart
+++ lib/menu/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:easy_search_bar/easy_search_bar.dart';
+
 
 class Home extends StatefulWidget {
   static const routeName = '/';
