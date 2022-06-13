6653ce81e67307d0b11db9588f5bbc5e2faee054
phonesavanhDEV <phonesavanh6912@gmail.com>
2022-06-13 15:55:20 +0700

    new

--- lib/menu/home.dart
+++ lib/menu/home.dart
@@ -14,6 +14,11 @@ class Home extends StatefulWidget {
 }
 
 final List<String> imgList = [
+  // 'assets/images/Jotun Nippon.jpg',
+  // 'assets/images/Hitachi.jpg',
+  // 'assets/images/Santa Monato.jpg',
+  // 'assets/images/Vandi.jpg',
+  // 'assets/images/Victoria Hamany.jpg',
   'https://www.souvanny.la/administrator/assets/slide_image/image_show_resize/Jotun_Nippon.jpeg',
   'https://www.souvanny.la/administrator/assets/slide_image/image_show_resize/Vandi.jpeg',
   'https://www.souvanny.la/administrator/assets/slide_image/image_show_resize/%E0%BA%81%E0%BA%B0%E0%BB%80%E0%BA%9A%E0%BA%B7%E0%BB%89%E0%BA%AD%E0%BA%87.jpeg',
@@ -95,65 +100,77 @@ class _HomeState extends State<Home> {
     //             onTap: () => Navigator.pop(context))
     //       ])),
     return MaterialApp(
-       theme: ThemeData(primarySwatch: Colors.deepOrange),
-       home: Scaffold(
-      appBar: EasySearchBar(
-        title: Text(''),
-        onSearch: (value) => setState(() => searchValue = value),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: Text("Home"),
-              accountEmail: Text("phonesavanh@gamil.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.white,
-                backgroundImage: AssetImage('assets/images/new_logo.png'),
-                // backgroundImage: NetworkImage(
-                //     "https://scontent.fbkk10-1.fna.fbcdn.net/v/t1.6435-9/118874943_3472399469478138_4332737009688307690_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGl-eWBoJyWxFHxUn3-m165oWr_wv-6EWGhav_C_7oRYechsZE9nfX5Q4emUJdrwmZbmzfO3sQgPmIL0ZVR0fIm&_nc_ohc=7U5rHKnENYIAX_lXO5g&_nc_ht=scontent.fbkk10-1.fna&oh=00_AT8mrTl1ntnFKUkeJGYF5f6KN0xX0k6KPJE28ok4Jb-cMg&oe=62CA9C74"),
-              ),
-              decoration: BoxDecoration(
-                color: Colors.deepOrange,
-                image: new DecorationImage(
-                  image: new ExactAssetImage('assets/images/bg01.jpg'),
-                  fit: BoxFit.fill,
+        theme: ThemeData(primarySwatch: Colors.deepOrange),
+        home: Scaffold(
+          appBar: EasySearchBar(
+            title: Text(''),
+            onSearch: (value) => setState(() => searchValue = value),
+          ),
+          drawer: Drawer(
+            child: ListView(
+              children: [
+                UserAccountsDrawerHeader(
+                  accountName: Text("Home"),
+                  accountEmail: Text("phonesavanh@gamil.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    backgroundImage: AssetImage('assets/images/new_logo.png'),
+                    // backgroundImage: NetworkImage(
+                    //     "https://scontent.fbkk10-1.fna.fbcdn.net/v/t1.6435-9/118874943_3472399469478138_4332737009688307690_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGl-eWBoJyWxFHxUn3-m165oWr_wv-6EWGhav_C_7oRYechsZE9nfX5Q4emUJdrwmZbmzfO3sQgPmIL0ZVR0fIm&_nc_ohc=7U5rHKnENYIAX_lXO5g&_nc_ht=scontent.fbkk10-1.fna&oh=00_AT8mrTl1ntnFKUkeJGYF5f6KN0xX0k6KPJE28ok4Jb-cMg&oe=62CA9C74"),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.deepOrange,
+                    image: new DecorationImage(
+                      image: new ExactAssetImage('assets/images/bg01.jpg'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
                 ),
-              ),
+                ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text(
+                    "ໜ້າຫຼັກ",
+                    style: TextStyle(fontFamily: 'NotoSansLao'),
+                  ),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.account_box),
+                  title: Text(
+                    "ກ່ຽວກັບ",
+                    style: TextStyle(fontFamily: 'NotoSansLao'),
+                  ),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.grid_3x3_outlined),
+                  title: Text(
+                    "ສິນຄ້າ",
+                    style: TextStyle(fontFamily: 'NotoSansLao'),
+                  ),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.contact_mail),
+                  title: Text(
+                    "ຕິດຕໍ່",
+                    style: TextStyle(fontFamily: 'NotoSansLao'),
+                  ),
+                  onTap: () {},
+                )
+              ],
             ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("Home"),
-              onTap: () {},
+          ),
+          body: Container(
+              child: CarouselSlider(
+            options: CarouselOptions(
+              aspectRatio: 2.0,
+              enlargeCenterPage: true,
+              scrollDirection: Axis.vertical,
+              autoPlay: true,
             ),
-            ListTile(
-              leading: Icon(Icons.account_box),
-              title: Text("About"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.grid_3x3_outlined),
-              title: Text("Products"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.contact_mail),
-              title: Text("Contact"),
-              onTap: () {},
-            )
-          ],
-        ),
-      ),
-      body: Container(
-          child: CarouselSlider(
-        options: CarouselOptions(
-          aspectRatio: 2.0,
-          enlargeCenterPage: true,
-          scrollDirection: Axis.vertical,
-          autoPlay: true,
-        ),
-        items: imageSliders,
-      )),
-    ));
+            items: imageSliders,
+          )),
+        ));
   }
 }
