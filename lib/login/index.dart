b5e1375b2ee46ab801b872222cb965365a9544cf
phonesavanhDEV <80586029+phonesavanhDEV@users.noreply.github.com>
2022-09-14 15:13:13 +0700

    .add

--- lib/login/index.dart
+++ lib/login/index.dart
@@ -1,6 +1,6 @@
-import 'dart:ui';
+// import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+// import 'package:lottie/lottie.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '/menu/launcher.dart';
 // void main()=>runApp(
